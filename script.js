16a990c13f3dce162d00992965abbec68490139b
Johanes Alberto <113593955+johanesashn@users.noreply.github.com>
2026-03-13 09:52:25 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// Get the button element
+const button = document.getElementById("myButton");
+
+// Add click event
+button.onclick = function() {
+    // Change the background color to pink
+    button.style.backgroundColor = 'pink';
+};
